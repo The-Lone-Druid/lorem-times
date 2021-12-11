abce8629d7ed84e714b69f67549e3d334cd59eba
The-Lone-Druid <zahid.786shaikh@gmail.com>
2021-12-11 23:14:15 +0530

    React with redux

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <Route exact path='/about' component={About} />
             <Route exact path='/contact' component={Contact} />
-            <Route exact path='/:id/:post_title' component={Post} />
+            <Route exact path='/:post_title' component={Post} />
           </Switch>
         </div>
       </BrowserRouter>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,34 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Pokeball from '../pokeball.png';
+import { connect } from 'react-redux';
 
 class Home extends Component {
 
-    state = {
-        posts: [ ]
-    }
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                this.setState({
-                    posts: res.data.slice(0,10)
-                });
-            })
-
-    }
-
     render() {
-
-        const {posts} = this.state;
+        const { posts } = this.props;
         const postList = posts.length ? (
             posts.map(post => {
                 return (
                     <div className='post card' key={post.id}>
                         <img src={Pokeball} alt="A pokeball" />
                         <div className="card-content">
-                            <Link to={`/${post.id}/${post.title}`}>
+                            <Link to={`/${post.title}`}>
                                 <span className='card-title red-text'>
                                     {post.title}
                                 </span>
@@ -55,4 +40,10 @@ class Home extends Component {
     }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+};
+
+export default connect(mapStateToProps)(Home)
